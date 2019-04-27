6e874748fa9652dac5556899dc279e98ac7bd8df
dance2die <sungmkim80@gmail.com>
2019-04-27 05:14:16 -0400

    added parcel

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { FixedSizeGrid as Grid } from 'react-window'
+import AutoSizer from 'react-virtualized-auto-sizer'
+
+const log = console.log
+
+log(`hi`)
+
+const Cell = ({ columnIndex, rowIndex, style }) => (
+  <div
+    className={
+      columnIndex % 2
+        ? rowIndex % 2 === 0
+          ? 'GridItemOdd'
+          : 'GridItemEven'
+        : rowIndex % 2
+        ? 'GridItemOdd'
+        : 'GridItemEven'
+    }
+    style={style}
+  >
+    r{rowIndex}, c{columnIndex}
+  </div>
+)
+
+const Example = () => (
+  <AutoSizer>
+    {({ height, width }) => (
+      <Grid
+        className='Grid'
+        height={height}
+        width={width}
+        columnCount={1000}
+        columnWidth={100}
+        rowCount={1000}
+        rowHeight={35}
+      >
+        {Cell}
+      </Grid>
+    )}
+  </AutoSizer>
+)
+
+render(<Example />, document.getElementById('root'))
