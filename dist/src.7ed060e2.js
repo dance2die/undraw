90f7f79ca062f7ef6603a0624e0e20d5a14d9202
dance2die <sungmkim80@gmail.com>
2019-04-27 21:25:36 -0400

    Added undraw images

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -28198,7 +28198,16 @@ AutoSizer.defaultProps = {
 };
 var _default = AutoSizer;
 exports.default = _default;
-},{"react":"../node_modules/react/index.js"}],"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js"}],"../src/names.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = ['3d_modeling.svg', 'about_me.svg', 'about_us_page.svg', 'absorbed_in.svg', 'accept_request.svg', 'accept_terms.svg', 'account.svg', 'active_support.svg', 'a_day_at_the_park.svg', 'add_files.svg', 'add_notes.svg', 'address.svg', 'add_to_cart.svg', 'add_user.svg', 'adjustments.svg', 'adventure.svg', 'agreement.svg', 'aircraft.svg', 'airport.svg', 'air_support.svg', 'alert.svg', 'algolia.svg', 'alien_science.svg', 'all_the_data.svg', 'analysis.svg', 'analytics.svg', 'analyze.svg', 'android.svg', 'apartment_rent.svg', 'app_installation.svg', 'appreciation.svg', 'around_the_world.svg', 'artificial_intelligence.svg', 'artist.svg', 'art_lover.svg', 'art.svg', 'astronaut.svg', 'at_work.svg', 'authentication.svg', 'autumn.svg', 'awards.svg', 'a_whole_year.svg', 'baby.svg', 'back_in_the_day.svg', 'back_to_school.svg', 'balloons.svg', 'barber.svg', 'basketball.svg', 'beach.svg', 'beer_celebration.svg', 'before_dawn.svg', 'begin_chat.svg', 'be_the_hero.svg', 'bibliophile.svg', 'birthday_cake.svg', 'bitcoin_p2p.svg', 'bitcoin.svg', 'blank_canvas.svg', 'blogging.svg', 'blog_post.svg', 'blooming.svg', 'booking.svg', 'book_lover.svg', 'bookmarks.svg', 'brainstorming.svg', 'breaking_barriers.svg', 'broadcast.svg', 'browser_stats.svg', 'buddies.svg', 'buffer.svg', 'bug_fixing.svg', 'building_blocks.svg', 'building.svg', 'business_analytics.svg', 'business_deal.svg', 'businessman.svg', 'business_plan.svg', 'businesswoman.svg', 'bus_stop.svg', 'by_my_car.svg', 'calculator.svg', 'calendar.svg', 'calling.svg', 'camera.svg', 'campfire.svg', 'camping.svg', 'cancel.svg', 'candidate.svg', 'career_progress.svg', 'cautious_dog.svg', 'celebration.svg', 'charts.svg', 'chat.svg', 'chatting.svg', 'checking_boxes.svg', 'checklist.svg', 'chef.svg', 'children.svg', 'chilling.svg', 'choice.svg', 'choose.svg', 'christmas_stocking.svg', 'christmas_tree.svg', 'circles.svg', 'city_driver.svg', 'city_girl.svg', 'cloud_hosting.svg', 'cloud_sync.svg', 'code_review.svg', 'code_typing.svg', 'coding.svg', 'coffee_break.svg', 'collaboration.svg', 'collab.svg', 'collecting.svg', 'collection.svg', 'community.svg', 'completed.svg', 'compose_music.svg', 'conference_speaker.svg', 'conference.svg', 'confirmation.svg', 'confirmed.svg', 'connected.svg', 'connected_world.svg', 'connecting_teams.svg', 'contact_us.svg', 'container_ship.svg', 'contemplating.svg', 'content_creator.svg', 'content.svg', 'contrast.svg', 'control_panel.svg', 'conversation.svg', 'convert.svg', 'cookie_love.svg', 'coolness.svg', 'country_side.svg', 'couple.svg', 'co-workers.svg', 'co-working.svg', 'create.svg', 'creation_process.svg', 'creation.svg', 'creative_experiment.svg', 'creative_team.svg', 'creative_thinking.svg', 'creative_woman.svg', 'creativity.svg', 'credit_card_payments.svg', 'credit_card_payment.svg', 'credit_card.svg', 'crypto_flowers.svg', 'customer_survey.svg', 'dark_alley.svg', 'dark_analytics.svg', 'dashboard.svg', 'data_points.svg', 'data_report.svg', 'data.svg', 'data_trends.svg', 'decide.svg', 'deconstructed.svg', 'deliveries.svg', 'delivery.svg', 'departing.svg', 'depi.svg', 'design_community.svg', 'designer_girl.svg', 'designer_life.svg', 'designer.svg', 'design_inspiration.svg', 'design_process.svg', 'design_thinking.svg', 'design_tools.svg', 'destination.svg', 'developer_activity.svg', 'development.svg', 'devices.svg', 'different_love.svg', 'digital_currency.svg', 'digital_nomad.svg', 'directions.svg', 'discount.svg', 'doctors.svg', 'doctor.svg', 'documents.svg', 'dog_walking.svg', 'doll_play.svg', 'domain_names.svg', 'done.svg', 'download.svg', 'dreamer.svg', 'drone_delivery.svg', 'drone_race.svg', 'dua_lipa.svg', 'easter_egg_hunt.svg', 'eating_together.svg', 'editable.svg', 'electric_car.svg', 'elements.svg', 'email_campaign.svg', 'email_capture.svg', 'emails.svg', 'empty.svg', 'envelope.svg', 'environment.svg', 'ethereum.svg', 'ether.svg', 'events.svg', 'everyday_design.svg', 'exams.svg', 'experts.svg', 'exploring.svg', 'fall_is_coming.svg', 'fans.svg', 'fashion_blogging.svg', 'fast_car.svg', 'fast_loading.svg', 'fatherhood.svg', 'features_overview.svg', 'feeling_blue.svg', 'festivities.svg', 'file_bundle.svg', 'file_searching.svg', 'files_sent.svg', 'file_synchronization.svg', 'filing_system.svg', 'filter.svg', 'finance.svg', 'financial_data.svg', 'fingerprint.svg', 'finish_line.svg', 'fireworks.svg', 'firmware.svg', 'fish_bowl.svg', 'fishing.svg', 'fitness_tracker.svg', 'floating.svg', 'flowers.svg', 'focus.svg', 'folder.svg', 'followers.svg', 'following.svg', 'follow_me_drone.svg', 'forever.svg', 'forgot_password.svg', 'for_sale.svg', 'freelancer.svg', 'friendship.svg', 'frozen_figure.svg', 'game_day.svg', 'gaming.svg', 'gardening.svg', 'gdpr.svg', 'getting_coffee.svg', 'gift_card.svg', 'gifts.svg', 'gift.svg', 'girls_just_wanna_have_fun.svg', 'goal.svg', 'going_up.svg', 'golden_gate_bridge.svg', 'good_doggy.svg', 'good_team.svg', 'grades.svg', 'graduation.svg', 'grandma.svg', 'grand_slam.svg', 'group_chat.svg', 'group_selfie.svg', 'growing.svg', 'growth_analytics.svg', 'hamburger.svg', 'hang_out.svg', 'happy_2019.svg', 'happy_birthday.svg', 'happy_feeling.svg', 'happy_women_day.svg', 'having_fun.svg', 'healthy_habit.svg', 'heartbroken.svg', 'hello.svg', 'high_five.svg', 'hiking.svg', 'hire.svg', 'hiring.svg', 'home_run.svg', 'horror_movie.svg', 'house_searching.svg', 'houses.svg', 'i_can_fly.svg', 'image_folder.svg', 'image_post.svg', 'images.svg', 'image_upload.svg', 'inbox_cleanup.svg', 'influencer.svg', 'in_love.svg', 'in_progress.svg', 'instant_support.svg', 'instruction_manual.svg', 'in_sync.svg', 'intense_feeling.svg', 'interaction_design.svg', 'internet_on_the_go.svg', 'interview.svg', 'in_the_office.svg', 'in_the_pool.svg', 'in_thought.svg', 'into_the_night.svg', 'investing.svg', 'invite.svg', 'japan.svg', 'jason_mask.svg', 'javascript_frameworks.svg', 'job_hunt.svg', 'jogging.svg', 'journey.svg', 'joyride.svg', 'judge.svg', 'knowledge.svg', 'laravel_and_vue.svg', 'late_at_night.svg', 'launching.svg', 'learning.svg', 'lighthouse.svg', 'live_collaboration.svg', 'loading.svg', 'load_more.svg', 'login.svg', 'logistics.svg', 'lost.svg', 'love_is_in_the_air.svg', 'love.svg', 'loving_it.svg', 'mailbox.svg', 'mail_sent.svg', 'mail.svg', 'maintenance.svg', 'make_it_rain.svg', 'maker_launch.svg', 'makeup_artist.svg', 'making_art.svg', 'map_dark.svg', 'map_light.svg', 'map.svg', 'marilyn.svg', 'marketing.svg', 'mathematics.svg', 'may_the_force.svg', 'media_player.svg', 'medicine.svg', 'meditating.svg', 'meditation.svg', 'meeting.svg', 'memory_storage.svg', 'mention.svg', 'message_sent.svg', 'messages.svg', 'messaging_fun.svg', 'messaging.svg', 'messenger.svg', 'metrics.svg', 'millennial_girl.svg', 'mindfulness.svg', 'mind_map.svg', 'mint_tea.svg', 'missed_chances.svg', 'mission_impossible.svg', 'mobile_apps.svg', 'mobile_app.svg', 'mobile_browsers.svg', 'mobile_life.svg', 'mobile_login.svg', 'mobile_marketing.svg', 'mobile_payments.svg', 'mobile.svg', 'mobile_testing.svg', 'modern_life.svg', 'modern_woman.svg', 'moment_to_remember.svg', 'monitor.svg', 'more_music.svg', 'morning_essentials.svg', 'motherhood.svg', 'movie_night.svg', 'moving_forward.svg', 'multitasking.svg', 'music.svg', 'my_password.svg', 'names.txt', 'navigation.svg', 'nerd.svg', 'new_message.svg', 'newsletter.svg', 'news.svg', 'night_calls.svg', 'ninja.svg', 'no_data.svg', 'notebook.svg', 'note_list.svg', 'notes.svg', 'not_found.svg', 'notify.svg', 'off_road.svg', 'old_day.svg', 'onboarding.svg', 'online_friends.svg', 'online_page.svg', 'online_shopping.svg', 'online.svg', 'online_wishes.svg', 'online_world.svg', 'on_the_office.svg', 'on_the_way.svg', 'opened.svg', 'open_source.svg', 'operating_system.svg', 'order_confirmed.svg', 'ordinary_day.svg', 'organize_photos.svg', 'organize_resume.svg', 'organizing_projects.svg', 'outer_space.svg', 'page_not_found.svg', 'pair_programming.svg', 'palette.svg', 'passing_by.svg', 'payments.svg', 'pedestrian_crossing.svg', 'pen_tool.svg', 'people_search.svg', 'personal_data.svg', 'personalization.svg', 'personal_notes.svg', 'personal_settings.svg', 'personal_site.svg', 'personal_text.svg', 'personal_trainer.svg', 'photo_album.svg', 'photocopy.svg', 'photo_sharing.svg', 'photos.svg', 'photo.svg', 'pie_chart.svg', 'pie_graph.svg', 'pilates.svg', 'pizza_sharing.svg', 'plain_credit_card.svg', 'playful_cat.svg', 'podcast_audience.svg', 'podcast.svg', 'portfolio.svg', 'portfolio_update.svg', 'posting_photo.svg', 'post_online.svg', 'posts.svg', 'post.svg', 'powerful.svg', 'preferences.svg', 'presentation.svg', 'press_play.svg', 'printing_invoices.svg', 'problem_solving.svg', 'processing.svg', 'process.svg', 'product_hunt.svg', 'product_teardown.svg', 'product_tour.svg', 'professor.svg', 'profile_data.svg', 'profile_pic.svg', 'profile.svg', 'programmer.svg', 'programming.svg', 'projections.svg', 'prototyping_process.svg', 'qa_engineers.svg', 'questions.svg', 'queue.svg', 'quitting_time.svg', 'raining.svg', 'react.svg', 'reading_list.svg', 'real-time_sync.svg', 'recording.svg', 'refreshing.svg', 'relaxation.svg', 'relaxing_at_home.svg', 'reminder.svg', 'report.svg', 'responsiveness.svg', 'responsive.svg', 'resume_folder.svg', 'resume.svg', 'revenue.svg', 'ride_a_bicycle.svg', 'rising.svg', 'road_sign.svg', 'robotics.svg', 'romantic_getaway.svg', 'running_wild.svg', 'safe.svg', 'santa_claus.svg', 'savings.svg', 'schedule.svg', 'science.svg', 'scooter.svg', 'scrum_board.svg', 'sculpting.svg', 'search_engines.svg', 'search.svg', 'secure_data.svg', 'secure_server.svg', 'security_on.svg', 'security.svg', 'segmentation.svg', 'segment.svg', 'select.svg', 'selfie.svg', 'selfie_time.svg', 'server_down.svg', 'server_status.svg', 'server.svg', 'settings.svg', 'setup_analytics.svg', 'setup.svg', 'setup_wizard.svg', 'shopping.svg', 'site_stats.svg', 'skateboarding.svg', 'sleep_analysis.svg', 'smart_home.svg', 'smartwatch.svg', 'smiley_face.svg', 'snowman.svg', 'social_dashboard.svg', 'social_growth.svg', 'social_ideas.svg', 'social_influencer.svg', 'social_life.svg', 'social_media.svg', 'social_networking.svg', 'social_share.svg', 'social_strategy.svg', 'social_thinking.svg', 'social_tree.svg', 'social_update.svg', 'software_engineer.svg', 'specs.svg', 'speech_to_text.svg', 'speed_test.svg', 'spreadsheets.svg', 'stability_ball.svg', 'starman.svg', 'startled.svg', 'startup_life.svg', 'static_assets.svg', 'statistics.svg', 'status_update.svg', 'staying_in.svg', 'step_to_the_sun.svg', 'street_food.svg', 'stripe_payments.svg', 'studying.svg', 'subscriber.svg', 'subway.svg', 'successful_purchase.svg', 'sunny_day.svg', 'superhero.svg', 'super_thank_you.svg', 'super_woman.svg', 'surfer.svg', 'swipe_profiles.svg', 'synchronize.svg', 'sync.svg', 'tabs.svg', 'taken.svg', 'taking_notes.svg', 'target.svg', 'task.svg', 'tasting.svg', 'teacher.svg', 'teaching.svg', 'team_page.svg', 'team_spirit.svg', 'team.svg', 'team_work.svg', 'teddy_bear.svg', 'texting.svg', 'thoughts.svg', 'through_the_desert.svg', 'throw_down.svg', 'timeline.svg', 'time_management.svg', 'to_do_list.svg', 'to_do.svg', 'together.svg', 'to_the_stars.svg', 'toy_car.svg', 'track_and_field.svg', 'transfer_files.svg', 'transfer_money.svg', 'travel_booking.svg', 'travelers.svg', 'traveling.svg', 'treasure.svg', 'trip.svg', 'true_friends.svg', 'tweetstorm.svg', 'typewriter.svg', 'typing.svg', 'unboxing.svg', 'under_construction.svg', 'update.svg', 'upgrade.svg', 'upload_image.svg', 'uploading.svg', 'upload.svg', 'upvote.svg', 'usability_testing.svg', 'user_flow.svg', 'vault.svg', 'vehicle_sale.svg', 'version_control.svg', 'video_call.svg', 'videographer.svg', 'virtual_reality.svg', 'visual_data.svg', 'voice_control.svg', 'voice_interface.svg', 'vr_chat.svg', 'walking_around.svg', 'walk_in_the_city.svg', 'wallet.svg', 'wall_post.svg', 'warning.svg', 'weather_app.svg', 'weather.svg', 'web_devices.svg', 'website_setup.svg', 'wedding.svg', 'welcome.svg', 'window_shopping.svg', 'windows.svg', 'wind_turbine.svg', 'winners.svg', 'winter_designer.svg', 'winter_olympics.svg', 'wireframing.svg', 'wishes.svg', 'wishlist.svg', 'witch.svg', 'woman.svg', 'women_day.svg', 'word_of_mouth.svg', 'wordpress.svg', 'work_chat.svg', 'working_late.svg', 'working_remotely.svg', 'working.svg', 'workout.svg', 'work_time.svg', 'world.svg', 'xmas_surprise.svg', 'yacht.svg', 'young_and_happy.svg', 'youtube_tutorial.svg'];
+exports.default = _default;
+},{}],"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
 function getBundleURLCached() {
@@ -28281,41 +28290,74 @@ var _reactWindow = require("react-window");
 
 var _reactVirtualizedAutoSizer = _interopRequireDefault(require("react-virtualized-auto-sizer"));
 
+var _names = _interopRequireDefault(require("./names"));
+
 require("./index.scss");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var log = console.log;
-log("hi");
+log("hi", _names.default);
 
-var Cell = function Cell(_ref) {
-  var columnIndex = _ref.columnIndex,
-      rowIndex = _ref.rowIndex,
+var Cell2 = function Cell2(_ref) {
+  var index = _ref.index,
       style = _ref.style;
-  return _react.default.createElement("div", {
-    className: columnIndex % 2 ? rowIndex % 2 === 0 ? 'GridItemOdd' : 'GridItemEven' : rowIndex % 2 ? 'GridItemOdd' : 'GridItemEven',
-    style: style
-  }, "r", rowIndex, ", c", columnIndex);
+  return _react.default.createElement("img", {
+    style: style,
+    src: "../images/undraw/".concat(_names.default[index]),
+    alt: "".concat(_names.default[index])
+  });
 };
 
 var Example = function Example() {
   return _react.default.createElement(_reactVirtualizedAutoSizer.default, null, function (_ref2) {
     var height = _ref2.height,
         width = _ref2.width;
-    return _react.default.createElement(_reactWindow.FixedSizeGrid, {
-      className: "Grid",
+    return _react.default.createElement(_reactWindow.FixedSizeList, {
+      className: "List",
       height: height,
       width: width,
-      columnCount: 1000,
-      columnWidth: 100,
-      rowCount: 1000,
-      rowHeight: 35
-    }, Cell);
+      itemCount: _names.default.length,
+      itemSize: 200
+    }, Cell2);
   });
-};
+}; // const Cell = ({ columnIndex, rowIndex, style }) => (
+//   <div
+//     className={
+//       columnIndex % 2
+//         ? rowIndex % 2 === 0
+//           ? 'GridItemOdd'
+//           : 'GridItemEven'
+//         : rowIndex % 2
+//           ? 'GridItemOdd'
+//           : 'GridItemEven'
+//     }
+//     style={style}
+//   >
+//     r{rowIndex}, c{columnIndex}
+//   </div>
+// )
+// const Example = () => (
+//   <AutoSizer>
+//     {({ height, width }) => (
+//       <Grid
+//         className='Grid'
+//         height={height}
+//         width={width}
+//         columnCount={3}
+//         rowCount={names.length}
+//         columnWidth={150}
+//         rowHeight={100}
+//       >
+//         {Cell2}
+//       </Grid>
+//     )}
+//   </AutoSizer>
+// )
+
 
 (0, _reactDom.render)(_react.default.createElement(Example, null), document.getElementById('root'));
-},{"react":"../node_modules/react/index.js","react-dom":"../node_modules/react-dom/index.js","react-window":"../node_modules/react-window/dist/index.esm.js","react-virtualized-auto-sizer":"../node_modules/react-virtualized-auto-sizer/dist/index.esm.js","./index.scss":"../src/index.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"react":"../node_modules/react/index.js","react-dom":"../node_modules/react-dom/index.js","react-window":"../node_modules/react-window/dist/index.esm.js","react-virtualized-auto-sizer":"../node_modules/react-virtualized-auto-sizer/dist/index.esm.js","./names":"../src/names.js","./index.scss":"../src/index.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -28343,7 +28385,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34105" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33587" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -1,47 +1,72 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { FixedSizeGrid as Grid } from 'react-window'
+import { FixedSizeGrid as Grid, FixedSizeList as List } from 'react-window'
 import AutoSizer from 'react-virtualized-auto-sizer'
+
+import names from './names'
 
 import './index.scss'
 
 const log = console.log
 
-log(`hi`)
+log(`hi`, names)
 
-const Cell = ({ columnIndex, rowIndex, style }) => (
-  <div
-    className={
-      columnIndex % 2
-        ? rowIndex % 2 === 0
-          ? 'GridItemOdd'
-          : 'GridItemEven'
-        : rowIndex % 2
-        ? 'GridItemOdd'
-        : 'GridItemEven'
-    }
+const Cell2 = ({ index, style }) => (
+  <img
     style={style}
-  >
-    r{rowIndex}, c{columnIndex}
-  </div>
+    src={`../images/undraw/${names[index]}`}
+    alt={`${names[index]}`}
+  />
 )
 
 const Example = () => (
   <AutoSizer>
     {({ height, width }) => (
-      <Grid
-        className='Grid'
+      <List
+        className='List'
         height={height}
         width={width}
-        columnCount={1000}
-        columnWidth={100}
-        rowCount={1000}
-        rowHeight={35}
+        itemCount={names.length}
+        itemSize={200}
       >
-        {Cell}
-      </Grid>
+        {Cell2}
+      </List>
     )}
   </AutoSizer>
 )
+
+// const Cell = ({ columnIndex, rowIndex, style }) => (
+//   <div
+//     className={
+//       columnIndex % 2
+//         ? rowIndex % 2 === 0
+//           ? 'GridItemOdd'
+//           : 'GridItemEven'
+//         : rowIndex % 2
+//           ? 'GridItemOdd'
+//           : 'GridItemEven'
+//     }
+//     style={style}
+//   >
+//     r{rowIndex}, c{columnIndex}
+//   </div>
+// )
+// const Example = () => (
+//   <AutoSizer>
+//     {({ height, width }) => (
+//       <Grid
+//         className='Grid'
+//         height={height}
+//         width={width}
+//         columnCount={3}
+//         rowCount={names.length}
+//         columnWidth={150}
+//         rowHeight={100}
+//       >
+//         {Cell2}
+//       </Grid>
+//     )}
+//   </AutoSizer>
+// )
 
 render(<Example />, document.getElementById('root'))
