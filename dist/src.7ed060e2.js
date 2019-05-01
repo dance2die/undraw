0daf4f8ecd39220768dd47fa915b00f4529775a1
dance2die <sungmkim80@gmail.com>
2019-04-30 23:48:15 -0400

    latest dist

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -30291,7 +30291,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41369" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33205" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
