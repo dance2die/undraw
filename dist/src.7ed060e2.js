5329d292f8cfcf0e439c5f7796beafa10024012c
dance2die <sungmkim80@gmail.com>
2019-04-30 23:27:23 -0400

    Hover shows a list of tags associated with the hovered image

--- dist/src.7ed060e2.js
+++ dist/src.7ed060e2.js
@@ -30014,20 +30014,44 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
 var Image = function Image(_ref) {
   var style = _ref.style,
       _ref$file = _ref.file,
       image = _ref$file.image,
       tags = _ref$file.tags,
       title = _ref$file.title;
+
+  var _useState = (0, _react.useState)(false),
+      _useState2 = _slicedToArray(_useState, 2),
+      hover = _useState2[0],
+      setHover = _useState2[1];
+
   return _react.default.createElement("section", {
-    className: "image"
-  }, _react.default.createElement("img", {
+    className: "image",
     style: style,
+    onMouseEnter: function onMouseEnter() {
+      return setHover(true);
+    },
+    onMouseLeave: function onMouseLeave() {
+      return setHover(false);
+    }
+  }, _react.default.createElement("img", {
+    style: {
+      width: '100%',
+      height: '100%'
+    },
     src: "../images/svg/".concat(image),
     alt: title
   }), _react.default.createElement("div", {
-    className: "overlay"
+    className: "overlay".concat(hover ? ' hover' : '')
   }, tags));
 };
 
--- src/components/Images/Cell.js
+++ src/components/Images/Cell.js
@@ -1,14 +1,27 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext } from 'react'
 
 import { getColumnCount } from '../../utils'
 import FileNamesContext from '../FileNamesContext'
 
-const Image = ({ style, file: { image, tags, title } }) => (
-  <section className='image'>
-    <img style={style} src={`../images/svg/${image}`} alt={title} />
-    <div className='overlay'>{tags}</div>
-  </section>
-)
+const Image = ({ style, file: { image, tags, title } }) => {
+  const [hover, setHover] = useState(false)
+
+  return (
+    <section
+      className='image'
+      style={style}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <img
+        style={{ width: '100%', height: '100%' }}
+        src={`../images/svg/${image}`}
+        alt={title}
+      />
+      <div className={`overlay${hover ? ' hover' : ''}`}>{tags}</div>
+    </section>
+  )
+}
 
 const Cell = width => ({ rowIndex, columnIndex, style }) => {
   const fileNames = useContext(FileNamesContext)
