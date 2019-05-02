55f7f9afc65351bc10143afa001269a6b6d3a7b3
dance2die <sungmkim80@gmail.com>
2019-05-01 21:50:50 -0400

    Added google font, 'Dosis'

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Search({ filterByQuery }) {
   return (
     <section className='search'>
-      <h1 className='title'>Search Images</h1>
+      <h1 className='title'>Search Undraw Images</h1>
       <form onSubmit={filterByQuery}>
         <input type='text' onChange={filterByQuery} placeholder='Search' />
       </form>
