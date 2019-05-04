ebb2986a2a839c054eec231bfd9c35e19bf8f335
dance2die <sungmkim80@gmail.com>
2019-05-04 17:16:21 -0400

    Replaced Parcel JS with Webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+}
+
+module.exports = config
