eb80a4342eb79ca6bd41431932fa5c94130de7af
dance2die <sungmkim80@gmail.com>
2019-05-10 23:27:11 -0400

    Added a hamburger menu #9

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from '@reach/router'
+
+import './Menu.scss'
 
 function Menu() {
-  return <div>This is MENU.........</div>
+  const [isClicked, setIsClicked] = useState(false)
+  const toggleLinks = () => setIsClicked(isClicked => !isClicked)
+
+  return (
+    <nav className='menu'>
+      <section
+        className={`hamburger${isClicked ? ' selected' : ''}`}
+        onClick={toggleLinks}
+      >
+        <div className='bar1' />
+        <div className='bar2' />
+        <div className='bar3' />
+      </section>
+      <section className='links'>
+        <Link to='/About'>About</Link>
+      </section>
+    </nav>
+  )
 }
 
 export default Menu
