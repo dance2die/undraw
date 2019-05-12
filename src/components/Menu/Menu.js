60be2016d2c7d61c14089b21e05b312a4c83ea00
dance2die <sungmkim80@gmail.com>
2019-05-12 01:19:43 -0400

    Created 'About' component
    #9

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -11,7 +11,7 @@ function Menu() {
     <nav className='menu'>
       <section className={`links${isClicked ? ' selected' : ''}`}>
         <Link to='/'>Home</Link>
-        <Link to='/About'>About</Link>
+        <Link to='/about'>About</Link>
       </section>
       <section
         className={`hamburger${isClicked ? ' selected' : ''}`}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { render } from 'react-dom'
 import { Router } from '@reach/router'
 
 import Home from './components/Home'
+import About from './components/About'
 
 import './styles/index.scss'
 
 function App() {
   return (
     <Router>
+      <About path='/about' />
       <Home path='/' />
     </Router>
   )
